1c921979245f11aceab583657b489dc5133b41c3
sandeepPangare <pangaresandeep96@gmail.com>
2022-08-17 20:54:43 +0530

    added:PositiveorNegative

---
+++ src/EvenorOdd.java
@@ -0,0 +1,13 @@
+public class EvenorOdd {
+    public static void main(String[] args) {
+        int n=12;
+        if(n%2==0)
+        {
+            System.out.println("n is Even");
+        }
+        else
+        {
+            System.out.println("n is odd");
+        }
+    }
+}
---
+++ src/com/bridgelabz/PositiveorNegative.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+public class PositiveorNegative {
+    public static void main(String[] args) {
+
+        int n=11;
+
+        if(n > 0)
+        {
+            System.out.println(n + "Positive Number");
+        }
+        else if(n < 0)
+        {
+            System.out.println(n + "Negative Number");
+        }
+        else
+        {
+            System.out.println("zero");
+        }
+
+    }
+}
